0c4cb0c0792b1b9ba42c063119a6d91ae0d9abb6
varshithshetty2004 <132539524+varshithshetty2004@users.noreply.github.com>
2025-07-29 23:15:13 +0530

    Create second_largest.py

---
+++ second_largest.py
@@ -0,0 +1,9 @@
+a=int(input("enter the num1"))
+b=int(input("entert the num2"))
+c= int(input("enter the num3"))
+if(a>b and ac) :
+    print("second largest no", a)
+elif(b>a and bc):
+    print("second largest no", b)
+else:
+    print("invalid")
