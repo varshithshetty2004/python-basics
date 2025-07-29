800b121f0338c3178c862fce92a9aec0d798a92e
varshithshetty2004 <132539524+varshithshetty2004@users.noreply.github.com>
2025-07-29 23:36:26 +0530

    Create leetcode367.py

---
+++ leetcode367.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num < 2:
+            return True
+
+        left, right = 2, num // 2
+
+        while left <= right:
+            mid = (left + right) // 2
+            square = mid * mid
+
+            if square == num:
+                return True
+            elif square < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
