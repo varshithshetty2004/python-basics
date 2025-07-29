96c42fe8445a0e89f432419dda2bc84acfa22321
varshithshetty2004 <132539524+varshithshetty2004@users.noreply.github.com>
2025-07-29 23:24:16 +0530

    Create coprime.py

---
+++ coprime.py
@@ -0,0 +1,15 @@
+def gcd(a,b):
+    while(b!=0):
+        temp = a%b
+        a= b
+        b=temp
+    return a
+
+def cop(a,b):
+    return gcd(a,b)==1
+num = int(input("enter the value :"))
+for i in range(1,num):
+    for j in range (1,i):
+        for k in range(1,j):
+            if(j*j + k*k ==i*i and cop(i,j) and cop(j,k) and cop(i,k)):
+                print(i,j,k)
